97636a0c642a14b198534644d41964e63559b0ea
Dave Moreno <mordavidj@gmail.com>
2021-04-27 13:33:13 -0600

    Start annotating and formatting Items

--- Slotting/Item.py
+++ Slotting/Item.py
@@ -1,23 +1,36 @@
+import numpy as np
+
 class Item():
-    def __init__(self):
+    '''Class to store item information for use within a pickface
+
+    '''
+    id: str
+    desc: str
+    case_qty: int
+    height: float
+    width: float
+    length: float
+
+    def __init__(self, **kwargs):
         self.id = None
-        self.name = None
         self.desc = None
-        self.case_qty = None
-        self.height = None
-        self.width = None
-        self.length = None
+        self.case_qty = np.nan
+        self.height = np.nan
+        self.width = np.nan
+        self.length = np.nan
+
+        self.id = self.set_id(kwargs.get('id'))
+
 
     def display(self):
+        print('')
         print(f'ID...........{self.id}')
-        print(f'Name.........{self.name}')
         print(f'Description..{self.desc}')
         print(f'Case Qty.....{self.case_qty}')
-        print(f'WxLxH........{self.width}x{self.length}x{self.height}')
+        print('WxLxH........{0:.2} x {1:.2} x {2:.2}'.format(self.width, self.length, self.height))
 
     def get_info(self):
         return {'id'        : self.id, 
-                'name'      : self.name, 
                 'desc'      : self.desc, 
                 'case_qty'  : self.case_qty, 
                 'height'    : self.height, 
@@ -29,12 +42,14 @@ class Item():
         return (self.width, self.length, self.height)
 
 
+    def set_dimensions(self, width, length, height):
+        self.set_width(width)
+        self.set_length(length)
+        self.set_height(height)
+
+
     def set_id(self, id):
         self.id = str(id)
-
-
-    def set_name(self, name):
-        self.name = str(name)
 
 
     def set_desc(self, desc):
@@ -45,12 +60,13 @@ class Item():
         try:
             case_qty = int(case_qty)
         except:
-            raise Exception('Item quantity must be an integer')
+            raise Exception('Case quantity must be an integer')
 
-        if case_qty < 0:
+        if case_qty > 0:
             self.case_qty = case_qty
         else:
-            raise Exception('Item quantity cannot be less than 0')
+            self.case_qty = np.nan
+            raise Exception('Case quantity cannot be less than 0')
 
 
     def set_height(self, height):
@@ -59,9 +75,10 @@ class Item():
         except:
             raise Exception('Item height must be a number')
 
-        if height < 0:
+        if height > 0:
             self.height = height
         else:
+            self.height = np.nan
             raise Exception('Item height cannot be less than 0') 
 
 
@@ -71,9 +88,10 @@ class Item():
         except:
             raise Exception('Item width must be a number')
 
-        if width < 0:
+        if width > 0:
             self.width = width
         else:
+            self.width = np.nan
             raise Exception('Item width cannot be less than 0')
 
 
@@ -83,7 +101,8 @@ class Item():
         except:
             raise Exception('Item length must be a number')
 
-        if length < 0:
+        if length > 0:
             self.length = length
         else:
+            self.length = np.nan
             raise Exception('Item length cannot be less than 0')
